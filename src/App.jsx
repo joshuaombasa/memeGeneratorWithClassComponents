8f4251cb6056ab3669329f07282b4290df5bd6c3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-08 23:58:52 +0300

    added jsx

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import './App.css'
+import Header from './Header'
+import Hero from './Hero'
+
+
+class App extends React.Component {
+
+  state = {
+    topText: '',
+    bottomText: '',
+    randomImg: "http://i.imgflip.com/1bij.jpg" ,
+    data: []
+  }
+
+  getRandomImage = (event) => {
+    event.preventDefault()
+    const randomNumber = Math.floor(Math.random() * this.state.data.length)
+    const randomUrl = this.state.data[randomNumber].url
+    
+    this.setState(preState => {
+      return {
+        ...preState,
+        randomImg: randomUrl
+      }
+    })
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState(preState => {
+      return {
+        ...preState,
+        [name]: value
+      }
+    })
+  }
+
+  handlSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  componentDidMount(){
+    fetch("https://api.imgflip.com/get_memes")
+       .then(res => res.json())
+       .then(data => {
+        // console.log(data.data.memes)
+        this.setState(preState => {
+          return {
+            ...preState,
+            data: data.data.memes
+          }
+        })
+       })
+  }
+
+
+render() {
+
+  console.log(this.state)
+
+  return (
+    <div className='container'>
+      <Header />
+      <Hero 
+      state={this.state} 
+      handleChange={this.handleChange}
+      getRandomImage={this.getRandomImage}
+      />
+    </div>
+  )
+}
+  
+}
+
+export default App
