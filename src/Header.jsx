4ca78a03e485d4b6c54b2bf3286276a5789f0641
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-08 23:59:04 +0300

    added jsx

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Header extends React.Component {
+
+    render() {
+        return (
+            <header className="top--section">
+                <h1 className="title">MemeGenerator</h1>
+            </header>
+        )
+    }
+}
